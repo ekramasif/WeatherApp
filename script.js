62e3a338824f60e1204616dd7ca84c0fd0d6889f
ekramasif <23asib43@gmail.com>
2021-08-04 15:21:05 +0600

    Updated

---
+++ script.js
@@ -0,0 +1,25 @@
+const apiKey = 'b6cc4392568a3586e950307c86a22bbd';
+const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
+
+const getWeatherData = city => {
+    const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => updateUI(data))
+}
+
+const searchBtn = document.getElementById('search_button');
+searchBtn.addEventListener('click', () => {
+    const inputCity = document.getElementById('city').value;
+    getWeatherData(inputCity)
+})
+
+const updateUI = data => {
+    document.getElementById('show_city').innerText = data.name || "Unknown Location!";
+    document.getElementById('show_temperature').innerText = data.main.temp;
+    document.getElementById('weather_status').innerText = data.weather[0].main;
+    document.getElementById('icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    document.getElementById('city').value = "";
+}
+
+getWeatherData('Chittagong');
